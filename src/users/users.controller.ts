98ce9e152817942a21ed174898db536ee385033f
Shahriyor <baxtiyarovich0102@gmail.com>
2025-06-28 01:37:22 +0500

    enrollment is ready

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards, Req, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+
+@ApiTags('users')
+@ApiBearerAuth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -32,5 +37,15 @@ export class UsersController {
     return this.usersService.remove(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Get('me/enrollments')
+  async getMyEnrollments(@Req() req) {
+    const userId = req.user.id;
+    const user = await this.usersService.findOneWithEnrolledCourses(userId);
+    if (!user || !user.enrolledCourses) {
+  throw new NotFoundException('Enrolled courses not found');
+  }
+    return user.enrolledCourses;
+  }
   
 }
