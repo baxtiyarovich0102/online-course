1e5de45b13de5519f74bab8822a4f4a5e8278fcd
Shahriyor <baxtiyarovich0102@gmail.com>
2025-06-29 02:53:44 +0500

    assignment qismi to'liq tugatildi

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -1,19 +1,20 @@
-import { Controller, Post, Param, Body, UseGuards, Req, ParseIntPipe, NotFoundException } from '@nestjs/common';
+import { Controller, Post, Param, Body, UseGuards, Req, ParseIntPipe, NotFoundException, Get } from '@nestjs/common';
 import { AssignmentsService } from './assignments.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateAssignmentDto } from './dto/create-assignment.dto';
 import { ModulesService } from 'src/modules/modules.service';
 import { EnrollmentGuard } from 'src/auth/guards/enrollment.guard';
 
-@Controller('modules/:moduleId/assignment')
-@UseGuards(JwtAuthGuard, EnrollmentGuard)
+@Controller('assignments')
+@UseGuards(JwtAuthGuard)
 export class AssignmentsController {
   constructor(
     private readonly assignmentsService: AssignmentsService,
     private readonly modulesService: ModulesService,
   ) {}
 
-  @Post()
+  @Post(':moduleId')
+  @UseGuards(EnrollmentGuard)
   async submit(
     @Param('moduleId', ParseIntPipe) moduleId: number,
     @Body() dto: CreateAssignmentDto,
@@ -21,10 +22,14 @@ export class AssignmentsController {
   ) {
     const user = req.user;
     const module = await this.modulesService.findOne(moduleId);
-
-    if (!module) {
-    throw new NotFoundException('Modul topilmadi');
-  }
+    if (!module) throw new NotFoundException('Modul topilmadi');
     return this.assignmentsService.create(dto, user, module);
   }
+
+  @Get('my')
+  async getMyAssignments(@Req() req) {
+    const userId = req.user.id;
+    return this.assignmentsService.findByUser(userId);
+  }
 }
+
